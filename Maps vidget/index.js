69726ce5eef801c5dd471f39574786ff809b4c06
AndriyStekhnovych <stekhnovychandriy@gmail.com>
2024-07-19 13:54:52 +0300

    first commit

---
+++ Maps vidget/index.js
@@ -0,0 +1,45 @@
+// Set map
+function initialize() {
+    var mapOptions = {
+        // Zoom of map on start
+        zoom: 10,
+        // Initial center cordinates on start (Kyiv)
+        center: new google.maps.LatLng(50.442171246530364, 30.535298041192867),
+        // Type of map (ROADMAP, SATELLITE, HYBRID, TERRAIN)
+        mapTypeId: google.maps.MapTypeId.SATELlITE,
+        // Minimum zoom of map
+        minZoom: 2
+    };
+
+    // Create a new map instance using provided options
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    //Create an info window to display location info
+    var infoWindow = new google.maps.InfoWindow();
+
+    // Create a marker for example: Stryi, Ukraine
+    var marker = new google.maps.Marker({
+        // Cordinates for Stryi, UKraine
+        position: new google.maps.LatLng(49.256, 23.8443),
+        // Attach the marker
+        map: map,
+        // Tooltip on hover
+        title: 'Stryi, Ukraine'
+
+    });
+
+    // Add click event listener for marker
+    marker.addListener('click', function() {
+        infoWindow.setContent(marker.title);
+        infoWindow.open(map, marker)
+    });
+
+    // Adjust map center when the window is resized
+    google.maps.event.addDomListener(window, 'resize', function(){
+        map.setCenter(mapOptions.center);
+    })
+
+};
+
+// Initial the map when window loading finished
+google.maps.event.addDomListener(window, 'load', initialize)
